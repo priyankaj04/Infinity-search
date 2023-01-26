b48466445aee831097b971535d2b73d5836cb616
priyankaj04 <priyankaj2002o4@gmail.com>
2023-01-26 17:51:25 +0530

    deployment

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ function App() {
   return (
     <div className="App">
       <Routes>
+        <Route path="/" element={<Login />} />
           <Route path="/login" element={<Login />} />
           <Route path="/home" element={<Home />} />
       </Routes>
